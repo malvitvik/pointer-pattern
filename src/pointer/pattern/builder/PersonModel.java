a187fd8f1bd75379905c748e2c18cb4c38eb26de
v.malinochka <g!tHuBv!+@1>
2019-10-02 15:06:30 +0300

    Update builder pattern

---
+++ src/pointer/pattern/builder/PersonModel.java
@@ -0,0 +1,8 @@
+package pointer.pattern.builder;
+
+public class PersonModel {
+    public String firstName = "Test";
+    public String lastName = "User";
+    public int height = 10;
+    public float weight = 15f;
+}
